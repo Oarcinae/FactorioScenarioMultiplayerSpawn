f49a755ba96d1c0c755f6449f726feb0f08442f5
Oarcinae <oarcinae@gmail.com>
2024-11-11 12:22:34 -0500

    Updating changelog.

--- migrations/oarc-mod-V2.1.10.lua
+++ migrations/oarc-mod-V2.1.10.lua
@@ -1,18 +1,19 @@
---Migrate the force_grass to force_tiles new setting!
+-- Migrate the force_grass to force_tiles new setting!
 if storage.ocfg.spawn_general.force_tiles == nil then
     storage.ocfg.spawn_general.force_tiles = true
     log("Updating spawn_general config with new 'force_tiles' setting.")
 end
 
--- Init OCFG surfaces for other planets if they don't exist.
+-- Make sure fulgora config is set up if it is missing.
 if script.active_mods["space-age"] ~= nil then
-    if (storage.ocfg.surfaces_config["fulgora"] == nil) or 
+    if (storage.ocfg.surfaces_config["fulgora"] == nil) or
         (storage.ocfg.surfaces_config["fulgora"].spawn_config.fill_tile == nil) then
         storage.ocfg.surfaces_config["fulgora"] =
         {
             spawn_config = FULGORA_SPAWN_CONFIG,
             starting_items = FULGORA_STARTER_ITEMS
         }
+        log("Updating fulgora config with new spawn_config and starting_items.")
     end
 end
 
@@ -22,11 +23,13 @@ if storage.ocfg.surfaces_config["nauvis"].spawn_config.fill_tile == nil then
         spawn_config = NAUVIS_SPAWN_CONFIG,
         starting_items = NAUVIS_STARTER_ITEMS
     }
+    log("Updating nauvis config with new spawn_config and starting_items.")
 end
 
 -- New startup setting that is also cached in storage.
 if storage.ocfg.gameplay.default_enable_secondary_spawns_on_other_surfaces == nil then
     storage.ocfg.gameplay.default_enable_secondary_spawns_on_other_surfaces = false
+    log("Updating gameplay config with new default_enable_secondary_spawns_on_other_surfaces setting.")
 end
 
 --Make sure new planets get init'd. No harm in running this multiple times.
@@ -34,6 +37,7 @@ SeparateSpawnsInitPlanets()
 
 -- Block spam. Highhly requested.
 game.technology_notifications_enabled = false
+log("Disabling technology notifications.")
 
 -- New global teleport queue for nil characters.
 if storage.nil_character_teleport_queue == nil then
