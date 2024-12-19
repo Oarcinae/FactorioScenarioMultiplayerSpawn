bd2512bc8aa3820b0a09c9a5a69b5c06be2c68d7
Oarcinae <oarcinae@gmail.com>
2024-12-19 09:40:12 -0500

    Remove remaining references to buddy_pairs. Remove experimental message warning.

--- migrations/oarc-mod-V2.1.19.lua
+++ migrations/oarc-mod-V2.1.19.lua
@@ -3,6 +3,7 @@ for _,spawns in pairs(storage.unique_spawns) do
     for _,spawn in pairs(spawns) do
         -- If the name is not nil, but the player doesn't exist, nil it out.
         if (spawn.buddy_name ~= nil) and (game.players[spawn.buddy_name] == nil) then
+            log("Removing non existent buddy from spawn info: " .. spawn.buddy_name)
             spawn.buddy_name = nil
         end
     end
